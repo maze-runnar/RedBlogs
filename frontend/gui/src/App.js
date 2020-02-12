33883b95406d1b03ff86d17ab0878ba1c82c6811
sundaram <dubeysundaram057@gmail.com>
2020-02-13 00:20:15 +0530

    adding initial auth in react-redux

--- frontend/gui/src/App.js
+++ frontend/gui/src/App.js
@@ -4,13 +4,19 @@ import {BrowserRouter as Router} from 'react-router-dom';
 import BaseRouter from './routes';
 import 'antd/dist/antd.css';
 import CustomLayout from './containers/Layout';
+import ActionButton from 'antd/lib/modal/ActionButton';
+import {connect} from 'react-redux';
+import * as actions from  './store/actions/auth';
 // import ArticleList from './containers/ArticleListView';
 class App extends Component {
+  componentDidMount() {
+    this.props.onTryAutoSignup();
+  }
   render(){
     return (
-      <div className="App">
+      <div>
         <Router>
-       <CustomLayout>
+       <CustomLayout {...this.props}>
           <BaseRouter />
        </CustomLayout>
        </Router>
@@ -18,4 +24,18 @@ class App extends Component {
     );
   }
 }
-export default App;
+
+const mapStateToProps =  state => {
+  return {
+    isAuthenticated : state.token !== null
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onTryAutoSignup: () => dispatch(actions.authCheckState())
+  }
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
