b17dd7f89365ed7bc7862d6e5e3d408dace5c210
sundaram <dubeysundaram057@gmail.com>
2020-02-12 11:21:18 +0530

    first

---
+++ frontend/gui/src/App.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import './App.css';
+import {BrowserRouter as Router} from 'react-router-dom';
+import BaseRouter from './routes';
+import 'antd/dist/antd.css';
+import CustomLayout from './containers/Layout';
+import ArticleList from './containers/ArticleListView';
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <Router>
+       <CustomLayout>
+          <BaseRouter />
+       </CustomLayout>
+       </Router>
+      </div>
+    );
+  }
+}
+export default App;
